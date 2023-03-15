349d607ef1b4888cf834d68a6ab1069df32e50ab
coccigabry <coccigab@gmail.com>
2023-03-15 15:11:53 +0100

    created account cards

---
+++ client/src/components/AccountCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AccountCard = () => {
+  return (
+    <div className='accountCardWrapper'>
+      <h3 className="accountNumber">
+        ACCOUNT 1612660
+      </h3>
+      <div className="accountBalanceContainer">
+        <p className="balanceValue">
+          Euro balance
+        </p>
+        <p className="balanceAmount">
+          €1,000,000
+        </p>
+      </div>
+      <div className="accountBalanceContainer">
+        <p className="balanceValue">
+          US Dollars balance
+        </p>
+        <p className="balanceAmount">
+          $20,000
+        </p>
+      </div>
+      <div className="balanceDate">
+        31/03/2021
+      </div>
+    </div>
+  )
+}
+
+export default AccountCard
--- client/src/pages/Balance.jsx
+++ client/src/pages/Balance.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import AccountCard from '../components/AccountCard'
+import Specs from '../components/Specs'
+import Charts from '../components/Charts'
 
 const Balance = () => {
   return (
-    <div>Balance</div>
+    <div className='balanceContainer'>
+
+      <div className="balanceWrapper">
+        <p className="balanceTitle">YOUR BALANCE</p>
+
+        <div className="accountCardContainer">
+          <AccountCard />
+          <AccountCard />
+        </div>
+
+        <div className="specsContainer">
+          <Specs />
+        </div>
+
+        <div className="ChartsContainer">
+          <Charts />
+          <Charts />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
